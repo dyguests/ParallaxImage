c27a648d23512b79ad67774aebb4c28f12714213
fanhl <fanhonglin@klicen.com>
2018-03-16 17:04:18 +0800

    偏移

--- library/src/main/java/com/fanhl/windowimageview/WindowImageView.kt
+++ library/src/main/java/com/fanhl/windowimageview/WindowImageView.kt
@@ -2,6 +2,7 @@ package com.fanhl.windowimageview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
 
 /**
  * desc: 橱窗ImageView
@@ -10,31 +11,46 @@ import android.util.AttributeSet
  * @author fanhl
  */
 class WindowImageView : android.support.v7.widget.AppCompatImageView {
-    constructor(context: Context) : super(context) {}
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context) : super(context)
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        scaleType = ImageView.ScaleType.MATRIX
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        recomputeImgMatrix()
+    }
 
     override fun setFrame(l: Int, t: Int, r: Int, b: Int): Boolean {
-        if (drawable != null) {
-            val matrix = imageMatrix
-
-            val scale: Float
-            val viewWidth = width - paddingLeft - paddingRight
-            val viewHeight = height - paddingTop - paddingBottom
-            val drawableWidth = drawable.intrinsicWidth
-            val drawableHeight = drawable.intrinsicHeight
-
-            scale = if (drawableWidth * viewHeight > drawableHeight * viewWidth) {
-                viewHeight.toFloat() / drawableHeight.toFloat()
-            } else {
-                viewWidth.toFloat() / drawableWidth.toFloat()
-            }
-
-            matrix.setScale(scale, scale)
-            imageMatrix = matrix
-        }
+        recomputeImgMatrix()
         return super.setFrame(l, t, r, b)
+    }
+
+    private fun recomputeImgMatrix() {
+
+        val drawable = drawable ?: return
+
+        val matrix = imageMatrix
+
+        val scale: Float
+        val viewWidth = width - paddingLeft - paddingRight
+        val viewHeight = height - paddingTop - paddingBottom
+        val drawableWidth = drawable.intrinsicWidth
+        val drawableHeight = drawable.intrinsicHeight
+
+        if (drawableWidth * viewHeight > drawableHeight * viewWidth) {
+            scale = viewHeight.toFloat() / drawableHeight.toFloat()
+        } else {
+            scale = viewWidth.toFloat() / drawableWidth.toFloat()
+        }
+
+        matrix.setScale(scale, scale)
+        imageMatrix = matrix
     }
 }
