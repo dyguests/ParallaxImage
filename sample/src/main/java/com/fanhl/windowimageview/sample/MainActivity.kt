31b9bd296863989e3602d787181096a34689c6dc
fanhl <fanhonglin@klicen.com>
2018-03-16 15:10:10 +0800

    local img

--- sample/src/main/java/com/fanhl/windowimageview/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/windowimageview/sample/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.fanhl.windowimageview.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val url = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Ophrys_apifera_flower1.jpg/170px-Ophrys_apifera_flower1.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
