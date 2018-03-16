0b0579691b341a9d6350c4337b54a39cccb4d7e8
fanhl <fanhonglin@klicen.com>
2018-03-16 15:58:34 +0800

    picasso

--- sample/src/main/java/com/fanhl/windowimageview/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/windowimageview/sample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.fanhl.windowimageview.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Picasso.get()
+                .load(url)
+                .into(imageView)
     }
 }
