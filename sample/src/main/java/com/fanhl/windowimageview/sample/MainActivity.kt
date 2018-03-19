d26217893c5d2937e5407f3a696c92da5ea0143b
fanhl <fanhonglin@klicen.com>
2018-03-19 14:00:00 +0800

    测试2

--- sample/src/main/java/com/fanhl/windowimageview/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/windowimageview/sample/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.fanhl.windowimageview.sample
 
-import android.os.Build
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
-import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
-import android.transition.TransitionManager
 import android.view.LayoutInflater
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val root = LayoutInflater.from(this).inflate(R.layout.activity_main, null) as ConstraintLayout
+        val root = LayoutInflater.from(this).inflate(R.layout.activity_main, null)
         setContentView(root)
 
         Picasso.get()
@@ -28,21 +24,5 @@ class MainActivity : AppCompatActivity() {
         Picasso.get()
                 .load(url)
                 .into(img_c)
-
-
-        var set = false
-        val constraint1 = ConstraintSet()
-        constraint1.clone(root)
-        val constraint2 = ConstraintSet()
-        constraint2.clone(this, R.layout.activity_main_alt)
-
-        fab.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                TransitionManager.beginDelayedTransition(root)
-                val constraint = if (set) constraint1 else constraint2
-                constraint.applyTo(root)
-                set = !set
-            }
-        }
     }
 }
