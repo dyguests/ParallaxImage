893b5ad7cd44b6d67f151a1bea6b99f0aebf5832
fanhl <dyguests@qq.com>
2019-03-31 21:16:15 +0800

    ParallaxImageGravitySensorHelper

--- sample/src/main/java/com/fanhl/parallaximage/sample/ui/gravity/GravityActivity.kt
+++ sample/src/main/java/com/fanhl/parallaximage/sample/ui/gravity/GravityActivity.kt
@@ -1,13 +1,18 @@
 package com.fanhl.parallaximage.sample.ui.gravity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fanhl.parallaximage.ParallaxImageGravitySensorHelper
 import com.fanhl.parallaximage.sample.R
+import kotlinx.android.synthetic.main.activity_gravity.*
 
 class GravityActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gravity)
+        ParallaxImageGravitySensorHelper.setup(img_cover1)
+        ParallaxImageGravitySensorHelper.setup(img_cover2)
+        ParallaxImageGravitySensorHelper.setup(img_cover3)
     }
 }
